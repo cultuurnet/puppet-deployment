6dbf888b730fe6ad7ac8a9f5fd3d5d6e49f0e5a5
Kristof Willaert <kristof.willaert@cultuurnet.be>
2016-04-18 17:51:48 +0200

    Add update_facts functionality to projectaanvraag deployment

--- manifests/projectaanvraag.pp
+++ manifests/projectaanvraag.pp
@@ -1,5 +1,15 @@
 class deployment::projectaanvraag (
-  $noop_deploy = false
+  $noop_deploy = false,
+  $update_facts = false,
+  $puppetdb_url = ''
 ) {
 
+  if $update_facts {
+    exec { 'update_facts':
+      command     => "/usr/local/bin/update_facts ${puppetdb_url}",
+      subscribe   => 'Package[omd]',
+      refreshonly => true,
+      noop        => $noop_deploy
+    }
+  }
 }
