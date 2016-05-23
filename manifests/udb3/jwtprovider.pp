93a01c962a5119c88fe7b6e9bf3e1cc442c1011a
Kristof Willaert <kristof.willaert@cultuurnet.be>
2016-05-23 15:17:44 +0200

    Add public and private keys to JWT provider deployment

--- manifests/udb3/jwtprovider.pp
+++ manifests/udb3/jwtprovider.pp
@@ -1,5 +1,7 @@
 class deployment::udb3::jwtprovider (
   $config_source,
+  $privkey_source,
+  $pubkey_source,
   $noop_deploy = false,
   $update_facts = false,
   $puppetdb_url = ''
@@ -19,6 +21,26 @@ class deployment::udb3::jwtprovider (
     group   => 'www-data',
     require => 'Package[udb3-jwt]',
     notify  => 'Class[Apache::Service]',
+    noop    => $noop_deploy
+  }
+
+  file { 'udb3-jwtprovider-privkey':
+    ensure  => 'file',
+    path    => '/var/www/udb-jwt-provider/private.pem',
+    source  => $privkey_source,
+    owner   => 'www-data',
+    group   => 'www-data',
+    require => 'Package[udb3-jwt]',
+    noop    => $noop_deploy
+  }
+
+  file { 'udb3-jwtprovider-pubkey':
+    ensure  => 'file',
+    path    => '/var/www/udb-jwt-provider/public.pem',
+    source  => $pubkey_source,
+    owner   => 'www-data',
+    group   => 'www-data',
+    require => 'Package[udb3-jwt]',
     noop    => $noop_deploy
   }
 
