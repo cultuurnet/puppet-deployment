b331ebcdc54efabcb7faad5d40966b4a4418fb58
Kristof Willaert <kristof.willaert@cultuurnet.be>
2016-05-26 11:29:37 +0200

    Add simple RabbitMQ deployment

---
+++ manifests/udb3/rabbitmq.pp
@@ -0,0 +1,33 @@
+class deployment::udb3::rabbitmq (
+  $admin_user,
+  $admin_password,
+  $vhost,
+  $noop_deploy = false,
+) {
+
+  class { 'rabbitmq':
+    manage_repos      => false,
+    delete_guest_user => true
+  }
+
+  rabbitmq_user { $admin_user:
+    admin    => true,
+    password => $admin_password,
+    noop     => $noop_deploy
+  }
+
+  rabbitmq_vhost { $vhost:
+    ensure => present,
+    noop   => $noop_deploy
+  }
+
+  rabbitmq_exchange { "cdbxml.vagrant.x.entry@${vhost}":
+    user        => $admin_user,
+    password    => $admin_password,
+    type        => 'topic',
+    internal    => false,
+    auto_delete => false,
+    durable     => true,
+    noop        => $noop_deploy
+  }
+}
