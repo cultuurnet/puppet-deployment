04a7b487ece12044bf7a8d8ac94088c938a74915
Kristof Willaert <kristof.willaert@cultuurnet.be>
2016-09-30 17:33:29 +0200

    Remove empty solr deployment class

--- manifests/solr.pp
+++
@@ -1,9 +0,0 @@
-class deployment::solr {
-
-  solr::url: 'http://archive.apache.org/dist/lucene/solr/'
-  solr::version: '4.9.0'
-  solr::solr_downloads: '/var/tmp/solr_downloads'
-  solr::java_home: '/usr/lib/jvm/java-8-oracle/jre'
-
-  # TODO: new module ?
-}
