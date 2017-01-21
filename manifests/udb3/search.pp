0d2113c4358ef45aa438962c4c08617a24aad6ab
Kristof Willaert <kristof.willaert@cultuurnet.be>
2017-01-21 10:16:54 +0100

    Add UDB3 search deployment class

---
+++ manifests/udb3/search.pp
@@ -0,0 +1,62 @@
+class deployment::udb3::search (
+  $config_source,
+  $noop_deploy = false,
+  $update_facts = false,
+  $puppetdb_url = ''
+) {
+
+  package { 'udb3-search':
+    ensure => 'latest',
+    notify => [ 'Class[Apache::Service]', 'Class[Supervisord::Service]'],
+    noop   => $noop_deploy
+  }
+
+  file { 'udb3-search-config':
+    ensure  => 'file',
+    path    => '/var/www/udb-search/config.yml',
+    source  => $config_source,
+    owner   => 'www-data',
+    group   => 'www-data',
+    require => 'Package[udb3-search]',
+    notify  => [ 'Class[Apache::Service]', 'Class[Supervisord::Service]'],
+    noop    => $noop_deploy
+  }
+
+  file { 'udb3-search-logdir':
+    ensure  => 'directory',
+    path    => '/var/www/udb-search/log',
+    owner   => 'www-data',
+    group   => 'www-data',
+    require => 'Package[udb3-search]',
+    noop    => $noop_deploy
+  }
+
+  logrotate::rule { 'udb3-search':
+    path          => '/var/www/udb-search/log/*.log',
+    rotate        => '10',
+    rotate_every  => 'day',
+    missingok     => true,
+    compress      => true,
+    delaycompress => true,
+    ifempty       => false,
+    create        => true,
+    create_mode   => '0640',
+    create_owner  => 'www-data',
+    create_group  => 'www-data',
+    sharedscripts => true,
+    postrotate    => '/usr/bin/supervisorctl restart udb3-search-service',
+    require       => 'File[udb3-search-logdir]',
+    noop          => $noop_deploy
+  }
+
+  if $update_facts {
+    exec { 'update_facts udb3 search':
+      command     => "/usr/local/bin/update_facts ${puppetdb_url}",
+      subscribe   => 'Package[udb3-search]',
+      refreshonly => true,
+      noop        => $noop_deploy
+    }
+  }
+
+  Class['php'] -> Class['deployment::udb3::search']
+}
