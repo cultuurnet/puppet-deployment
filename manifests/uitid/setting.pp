82bfe20ba305f351fa2f2d0e38ae556c0ab2ff01
Kristof Willaert <kristof.willaert@cultuurnet.be>
2017-05-19 17:53:47 +0200

    Use correct names for exec resources

--- manifests/uitid/setting.pp
+++ manifests/uitid/setting.pp
@@ -7,14 +7,14 @@ define deployment::uitid::setting (
 ) {
 
   if $ensure == 'absent' {
-    exec { "SAPI setting ${title}: absent":
+    exec { "UiTID setting ${title}: absent":
       command => "mysql --defaults-extra-file=/root/.my.cnf -e \"delete from ${database}.SETTING where K = '${title}';\"",
       path    => [ '/usr/local/bin', '/usr/bin', '/bin'],
       onlyif  => "test 0 != $(mysql --defaults-extra-file=/root/.my.cnf -s --skip-column-names -e \"select count(*) from ${database}.SETTING where K = '${title}';\")"
     }
   }
   else {
-    exec { "SAPI setting ${title}: ${value}":
+    exec { "UiTID setting ${title}: ${value}":
       command => "mysql --defaults-extra-file=/root/.my.cnf -e \"insert into ${database}.SETTING (K, TYPE, V, DTYPE) values ('${title}', ${type}, '${value}', '${dtype}') on duplicate key update CONTENT = '${value}';\"",
       path    => [ '/usr/local/bin', '/usr/bin', '/bin'],
       onlyif  => "test '${value}' != \"$(mysql --defaults-extra-file=/root/.my.cnf -s --skip-column-names -e \"select V from ${database}.SETTING where K = '${title}';\")\""
