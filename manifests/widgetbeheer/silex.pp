3ea75b213a82811ffb682641b9f9e563841b6c0d
Kristof Willaert <kristof.willaert@cultuurnet.be>
2017-08-10 14:02:12 +0200

    Add user_roles.yml and integration_types.yml to widgetbeheer

--- manifests/widgetbeheer/silex.pp
+++ manifests/widgetbeheer/silex.pp
@@ -1,5 +1,7 @@
 class deployment::widgetbeheer::silex (
   $config_source,
+  $user_roles_source,
+  $integration_types_source,
   $noop_deploy = false,
   $update_facts = false,
   $puppetdb_url = ''
@@ -17,6 +19,28 @@ class deployment::widgetbeheer::silex (
     ensure  => 'file',
     path    => '/var/www/widgetbeheer-api/config.yml',
     source  => $config_source,
+    owner   => 'www-data',
+    group   => 'www-data',
+    require => Package['widgetbeheer-silex'],
+    notify  => Class['apache::service'],
+    noop    => $noop_deploy
+  }
+
+  file { 'widgetbeheer-silex-user_roles':
+    ensure  => 'file',
+    path    => '/var/www/widgetbeheer-api/user_roles.yml',
+    source  => $user_roles_source,
+    owner   => 'www-data',
+    group   => 'www-data',
+    require => Package['widgetbeheer-silex'],
+    notify  => Class['apache::service'],
+    noop    => $noop_deploy
+  }
+
+  file { 'widgetbeheer-silex-integration_types':
+    ensure  => 'file',
+    path    => '/var/www/widgetbeheer-api/integration_types.yml',
+    source  => $integration_types_source,
     owner   => 'www-data',
     group   => 'www-data',
     require => Package['widgetbeheer-silex'],
