6177ae698def6c21529de227fffbb68dfa6f3521
Kristof Willaert <kristof.willaert@cultuurnet.be>
2017-10-25 16:45:37 +0200

    Add global UDB3 deployment class

---
+++ manifests/udb3.pp
@@ -0,0 +1,15 @@
+class deployment::udb3 {
+
+  contain deployment::udb3::rabbitmq
+
+  unless noop_deploy {
+    contain deployment::udb3::silex
+    contain deployment::udb3::angular
+    contain deployment::udb3::cdbxml
+    contain deployment::udb3::jwtprovider
+    contain deployment::udb3::swagger
+    contain deployment::udb3::uitpas
+    contain deployment::udb3::search
+    contain deployment::udb3::iis
+  }
+}
