7d92f89046e22b3e6a971494a3092711ceec5756
Kristof Willaert <kristof.willaert@cultuurnet.be>
2017-12-19 10:41:12 +0100

    Add widgetbeheer script to force addition of text/css type to index.html

--- manifests/widgetbeheer/angular.pp
+++ manifests/widgetbeheer/angular.pp
@@ -9,6 +9,26 @@ class deployment::widgetbeheer::angular (
     noop   => $noop_deploy
   }
 
+  package { 'rubygem-nokogiri':
+    ensure => 'installed'
+  }
+
+  file { 'add_text_css_type':
+    ensure => 'file',
+    source => 'puppet:///modules/deployment/widgetbeheer/add_text_css_type',
+    path   => '/usr/local/bin/add_text_css_type',
+    mode   => '0755'
+  }
+
+  exec { 'add_text_css_type':
+    command     => 'add_text_css_type /var/www/widgetbeheer/index.html > /var/www/widgetbeheer/index.html',
+    path        => [ '/usr/local/bin', '/usr/bin', '/bin'],
+    refreshonly => true,
+    require     => [ File['add_text_css_type'], Package['rubygem-nokogiri']],
+    subscribe   => Package['widgetbeheer-angular-app'],
+    noop        => $noop_deploy
+  }
+
   deployment::versions { $title:
     project      => 'widgetbeheer',
     packages     => 'widgetbeheer-angular-app',
