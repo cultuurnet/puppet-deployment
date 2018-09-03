d25295d3526729c7fffbcb29790df4a0055f48d2
Kristof Willaert <kristof.willaert@cultuurnet.be>
2018-09-03 12:32:21 +0200

    Parameterize udb3 angular package names

--- manifests/udb3/angular.pp
+++ manifests/udb3/angular.pp
@@ -1,6 +1,9 @@
 class deployment::udb3::angular (
   $config_source,
   $deploy_config_source = 'puppet:///modules/deployment/angular/angular-deploy-config.rb',
+  $project_prefix = 'udb3',
+  $app_package_name = 'udb3-angular-app',
+  $lib_package_name = 'udb3-angular',
   $noop_deploy = false,
   $update_facts = false,
   $puppetdb_url = ''
@@ -8,23 +11,23 @@ class deployment::udb3::angular (
 
   contain deployment
 
-  package { 'udb3-angular-app':
+  package { $app_package_name:
     ensure => 'latest',
     noop   => $noop_deploy
   }
 
-  package { 'udb3-angular':
+  package { $lib_package_name:
     ensure => 'latest',
     noop   => $noop_deploy
   }
 
   file { 'udb3-angular-app-config':
-    ensure => 'file',
-    path   => '/var/www/udb-app/config.json',
-    source => $config_source,
+    ensure  => 'file',
+    path    => '/var/www/udb-app/config.json',
+    source  => $config_source,
     owner   => 'www-data',
     group   => 'www-data',
-    require => 'Package[udb3-angular-app]',
+    require => Package[$app_package_name],
     noop    => $noop_deploy
   }
 
@@ -39,15 +42,15 @@ class deployment::udb3::angular (
   exec { 'angular-deploy-config':
     command     => 'angular-deploy-config /var/www/udb-app',
     path        => [ '/usr/local/bin', '/usr/bin', '/bin'],
-    subscribe   => [ 'Package[udb3-angular-app]', 'File[udb3-angular-app-config]', 'File[udb3-angular-app-deploy-config]'],
+    subscribe   => [ Package[$app_package_name], File['udb3-angular-app-config'], File['udb3-angular-app-deploy-config']],
     refreshonly => true,
     require     => Class['deployment'],
     noop        => $noop_deploy
   }
 
   deployment::versions { $title:
-    project      => 'udb3',
-    packages     => [ 'udb3-angular-app', 'udb3-angular'],
+    project      => $project_prefix,
+    packages     => [ $app_package_name, $lib_package_name],
     noop_deploy  => $noop_deploy,
     update_facts => $update_facts,
     puppetdb_url => $puppetdb_url
