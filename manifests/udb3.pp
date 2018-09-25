d24b64396fd62426f1f708e0a177ea7ad06ce34f
Kristof Willaert <kristof.willaert@cultuurnet.be>
2018-09-25 13:46:36 +0200

    Simplify conditional

--- manifests/udb3.pp
+++ manifests/udb3.pp
@@ -15,37 +15,7 @@ class deployment::udb3 (
     contain deployment::udb3::rabbitmq
   }
 
-  if $environment == 'development' {
-    unless $facts['noop_deploy'] == 'true' {
-      if $with_silex {
-        contain deployment::udb3::silex
-      }
-      if $with_angular {
-        contain deployment::udb3::angular
-      }
-      if $with_cdbxml {
-        contain deployment::udb3::cdbxml
-      }
-      if $with_jwtprovider {
-        contain deployment::udb3::jwtprovider
-      }
-      if $with_apidoc {
-        contain deployment::udb3::apidoc
-      }
-      if $with_uitpas {
-        contain deployment::udb3::uitpas
-      }
-      if $with_search {
-        contain deployment::udb3::search
-      }
-      if $with_iis {
-        contain deployment::udb3::iis
-      }
-      if $with_movie_api_fetcher {
-        contain deployment::udb3::movie_api_fetcher
-      }
-    }
-  } else {
+  unless $facts['noop_deploy'] == 'true' {
     if $with_silex {
       contain deployment::udb3::silex
     }
