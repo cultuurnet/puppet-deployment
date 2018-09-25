830b669cd316f66db2052bcfbee5de9fcfec83c1
Kristof Willaert <kristof.willaert@cultuurnet.be>
2018-09-25 17:33:41 +0200

    Allow several angular instances

--- manifests/udb3/angular.pp
+++ manifests/udb3/angular.pp
@@ -1,59 +1,17 @@
 class deployment::udb3::angular (
-  $config_source,
   $deploy_config_source = 'puppet:///modules/deployment/angular/angular-deploy-config.rb',
-  $project_prefix = 'udb3',
-  $app_package_name = 'udb3-angular-app',
-  $lib_package_name = 'udb3-angular',
-  $app_rootdir = '/var/www/udb-app',
-  $noop_deploy = false,
-  $update_facts = false,
-  $puppetdb_url = ''
+  $app_rootdir          = '/var/www/udb-app',
+  $instances            = {}
 ) {
 
   contain deployment
-
-  package { $app_package_name:
-    ensure => 'latest',
-    noop   => $noop_deploy
-  }
-
-  package { $lib_package_name:
-    ensure => 'latest',
-    noop   => $noop_deploy
-  }
-
-  file { 'udb3-angular-app-config':
-    ensure  => 'file',
-    path    => "${app_rootdir}/config.json",
-    source  => $config_source,
-    owner   => 'www-data',
-    group   => 'www-data',
-    require => Package[$app_package_name],
-    noop    => $noop_deploy
-  }
 
   file { 'udb3-angular-app-deploy-config':
     ensure => 'file',
     path   => '/usr/local/bin/angular-deploy-config',
     source => $deploy_config_source,
-    mode   => '0755',
-    noop   => $noop_deploy
+    mode   => '0755'
   }
 
-  exec { 'angular-deploy-config':
-    command     => "angular-deploy-config ${app_rootdir}",
-    path        => [ '/usr/local/bin', '/usr/bin', '/bin'],
-    subscribe   => [ Package[$app_package_name], File['udb3-angular-app-config'], File['udb3-angular-app-deploy-config']],
-    refreshonly => true,
-    require     => Class['deployment'],
-    noop        => $noop_deploy
-  }
-
-  deployment::versions { $title:
-    project      => $project_prefix,
-    packages     => [ $app_package_name, $lib_package_name],
-    noop_deploy  => $noop_deploy,
-    update_facts => $update_facts,
-    puppetdb_url => $puppetdb_url
-  }
+  create_resources('deployment::udb3::angular::instance', $instances)
 }
---
+++ manifests/udb3/angular/instance.pp
@@ -0,0 +1,51 @@
+define deployment::udb3::angular::instance (
+  $config_source,
+  $app_package_name,
+  $lib_package_name,
+  $app_rootdir,
+  $project_prefix = $title,
+  $noop_deploy = false,
+  $update_facts = false,
+  $puppetdb_url = ''
+) {
+
+  contain deployment
+  contain deployment::udb3::angular::common
+
+  package { $app_package_name:
+    ensure => 'latest',
+    noop   => $noop_deploy
+  }
+
+  package { $lib_package_name:
+    ensure => 'latest',
+    noop   => $noop_deploy
+  }
+
+  file { "${title}-angular-app-config":
+    ensure  => 'file',
+    path    => "${app_rootdir}/config.json",
+    source  => $config_source,
+    owner   => 'www-data',
+    group   => 'www-data',
+    require => Package[$app_package_name],
+    noop    => $noop_deploy
+  }
+
+  exec { "${title}-angular-deploy-config":
+    command     => "angular-deploy-config ${app_rootdir}",
+    path        => [ '/usr/local/bin', '/usr/bin', '/bin'],
+    subscribe   => [ Package[$app_package_name], File["${title}-angular-app-config"], Class['deployment::udb3::angular::common']],
+    refreshonly => true,
+    require     => Class['deployment'],
+    noop        => $noop_deploy
+  }
+
+  deployment::versions { $title:
+    project      => $project_prefix,
+    packages     => [ $app_package_name, $lib_package_name],
+    noop_deploy  => $noop_deploy,
+    update_facts => $update_facts,
+    puppetdb_url => $puppetdb_url
+  }
+}
