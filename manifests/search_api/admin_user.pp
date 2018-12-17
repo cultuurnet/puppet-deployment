2e33959ecd21a15743c7f682af4ea5cf99cf4fe1
Kristof Willaert <kristof.willaert@cultuurnet.be>
2018-12-17 13:10:00 +0100

    We need quoted strings in SQL

--- manifests/search_api/admin_user.pp
+++ manifests/search_api/admin_user.pp
@@ -4,7 +4,7 @@ define deployment::search_api::admin_user (
 ) {
 
   exec { "SAPI admin user ${title}":
-    command => "mysql --defaults-extra-file=/root/.my.cnf -e \"insert into ${database}.ADMINUSER values (${uid}, null, null);\"",
+    command => "mysql --defaults-extra-file=/root/.my.cnf -e \"insert into ${database}.ADMINUSER values ('${uid}', null, null);\"",
     path    => [ '/usr/local/bin', '/usr/bin', '/bin'],
     onlyif  => "test 0 -eq \"$(mysql --defaults-extra-file=/root/.my.cnf -s --skip-column-names -e \"select count(*) from ${database}.ADMINUSER where UID = '${uid}';\")\"",
     require => Class['mysql::server']
