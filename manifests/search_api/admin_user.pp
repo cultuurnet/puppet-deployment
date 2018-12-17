464b776aa028d6bf7432cd92dbb5cae7a2aee352
Kristof Willaert <kristof.willaert@cultuurnet.be>
2018-12-17 13:06:12 +0100

    Reverse condition for SAPI admin user

--- manifests/search_api/admin_user.pp
+++ manifests/search_api/admin_user.pp
@@ -6,7 +6,7 @@ define deployment::search_api::admin_user (
   exec { "SAPI admin user ${title}":
     command => "mysql --defaults-extra-file=/root/.my.cnf -e \"insert into ${database}.ADMINUSER values (${uid}, null, null);\"",
     path    => [ '/usr/local/bin', '/usr/bin', '/bin'],
-    onlyif  => "test 0 -ne \"$(mysql --defaults-extra-file=/root/.my.cnf -s --skip-column-names -e \"select count(*) from ${database}.ADMINUSER where UID = '${uid}';\")\"",
+    onlyif  => "test 0 -eq \"$(mysql --defaults-extra-file=/root/.my.cnf -s --skip-column-names -e \"select count(*) from ${database}.ADMINUSER where UID = '${uid}';\")\"",
     require => Class['mysql::server']
   }
 }
