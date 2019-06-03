bb1e326ad71e7b510c8dad84e12c4b4c0c0cb0eb
Kristof Willaert <kristof.willaert@cultuurnet.be>
2019-06-03 23:34:57 +0200

    Refactor uitiduitpas deployment to use profiles::glassfish

--- manifests/uitiduitpas.pp
+++ manifests/uitiduitpas.pp
@@ -22,7 +22,7 @@ class deployment::uitiduitpas (
   $payara_default_start_heap = '512m'
   $payara_default_max_heap = '512m'
 
-  include java8
+  contain profiles::glassfish
 
   Jvmoption {
     ensure       => 'present',
@@ -42,38 +42,12 @@ class deployment::uitiduitpas (
     notify       => Exec["restart_service_${service_name}"]
   }
 
-  class { 'glassfish':
-    install_method      => 'package',
-    package_prefix      => 'payara',
-    version             => '4.1.1.171.1',
-    create_service      => false,
-    enable_secure_admin => false,
-    manage_java         => false,
-    parent_dir          => '/opt',
-    install_dir         => 'payara',
-    require             => [ Class['apt::update'], Class['java8']]
-  }
-
-  package { 'mysql-connector-java':
-    ensure => 'latest',
-    notify => Exec["restart_service_${service_name}"]
-  }
-
-  # Hack to circumvent dependency problems with using glassfish::install_jars
-  file { 'mysql-connector-java':
-    ensure    => 'link',
-    path      => '/opt/payara/glassfish/lib/mysql-connector-java.jar',
-    target    => '/opt/mysql-connector-java/mysql-connector-java.jar',
-    require   => Class['glassfish::install'],
-    subscribe => Package['mysql-connector-java']
-  }
-
   glassfish::create_domain { $payara_domain:
     portbase       => $payara_portbase,
     service_name   => $service_name,
     create_service => true,
     start_domain   => true,
-    require        => File['mysql-connector-java']
+    require        => [ File['mysql-connector-java'], Class['profiles::glassfish'] ]
   }
 
   # This will only work if the default start heap value (512m) is present in
