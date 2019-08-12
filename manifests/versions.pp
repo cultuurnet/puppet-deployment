8c4f0e11d8e40aafe2eda760940e76587a5c42fe
Kristof Willaert <kristof.willaert@cultuurnet.be>
2019-08-12 22:29:35 +0200

    Escape dots in jq queries

--- manifests/versions.pp
+++ manifests/versions.pp
@@ -20,14 +20,14 @@ define deployment::versions (
     }
 
     exec { "update versions endpoint for package ${package}":
-      command     => "/opt/puppetlabs/bin/facter -pj ${project}_version | /usr/bin/jq .${project}_version > ${destination_dir}/versions.${project}",
+      command     => "/opt/puppetlabs/bin/facter -pj ${project}_version | /usr/bin/jq '.[\"${project}_version\"]' > ${destination_dir}/versions.${project}",
       subscribe   => Package[$package],
       refreshonly => true,
       noop        => $noop_deploy
     }
 
     exec { "update versions.${package} endpoint for package ${package}":
-      command     => "/opt/puppetlabs/bin/facter -pj ${project}_version.${package} | /usr/bin/jq .${project}_version.${package} > ${destination_dir}/versions.${project}.${package}",
+      command     => "/opt/puppetlabs/bin/facter -pj ${project}_version.${package} | /usr/bin/jq '.[\"${project}_version.${package}\"]' > ${destination_dir}/versions.${project}.${package}",
       subscribe   => Package[$package],
       refreshonly => true,
       noop        => $noop_deploy
