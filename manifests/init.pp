0c5d6cb400888fe174a5ab7d9252b8dfc5b59938
Kristof Willaert <kristof.willaert@cultuurnet.be>
2019-08-12 22:03:47 +0200

    Prepare move of versions to profiles module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,25 @@
 # Class: deployment
 class deployment {
 
-  contain ruby
-
-  file { 'update_facts':
-    ensure => 'file',
-    path   => '/usr/local/bin/update_facts',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => 'puppet:///modules/deployment/update_facts'
+  if ! defined(File['update_facts']) {
+    file { 'update_facts':
+      ensure => 'file',
+      path   => '/usr/local/bin/update_facts',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755',
+      source => 'puppet:///modules/deployment/update_facts'
+    }
   }
 
   file { 'get_fact_value':
-    ensure => 'file',
+    ensure => 'absent',
     path   => '/usr/local/bin/get_fact_value',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => 'puppet:///modules/deployment/get_fact_value'
+  }
+
+  if ! defined(Package['jq']) {
+    package { 'jq':
+      ensure => 'installed'
+    }
   }
 }
--- manifests/versions.pp
+++ manifests/versions.pp
@@ -14,24 +14,21 @@ define deployment::versions (
       exec { "update_facts for ${package} package":
         command     => "/usr/local/bin/update_facts ${puppetdb_url}",
         subscribe   => [ Package[$package], File['update_facts']],
-        require     => Class['deployment'],
         refreshonly => true,
         noop        => $noop_deploy
       }
     }
 
     exec { "update versions endpoint for package ${package}":
-      command     => "/usr/local/bin/get_fact_value ${project}_version > ${destination_dir}/versions.${project}",
-      subscribe   => [ Package[$package], File['get_fact_value']],
-      require     => Class['deployment'],
+      command     => "/opt/puppetlabs/bin/facter -pj ${project}_version | /usr/bin/jq .${project}_version > ${destination_dir}/versions.${project}",
+      subscribe   => Package[$package],
       refreshonly => true,
       noop        => $noop_deploy
     }
 
     exec { "update versions.${package} endpoint for package ${package}":
-      command     => "/usr/local/bin/get_fact_value ${project}_version.${package} > ${destination_dir}/versions.${package}",
-      subscribe   => [ Package[$package], File['get_fact_value']],
-      require     => Class['deployment'],
+      command     => "/opt/puppetlabs/bin/facter -pj ${project}_version.${package} | /usr/bin/jq .${project}_version.${package} > ${destination_dir}/versions.${project}.${package}",
+      subscribe   => Package[$package],
       refreshonly => true,
       noop        => $noop_deploy
     }
