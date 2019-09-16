355aaabd10ccdf745c2a8e371879feb4e8d33283
Kristof Willaert <kristof.willaert@cultuurnet.be>
2019-09-16 14:14:03 +0200

    Move update_facts to profiles module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,6 @@
 # Class: deployment
 class deployment {
 
-  if ! defined(File['update_facts']) {
-    file { 'update_facts':
-      ensure => 'file',
-      path   => '/usr/local/bin/update_facts',
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0755',
-      source => 'puppet:///modules/deployment/update_facts'
-    }
-  }
-
   file { 'get_fact_value':
     ensure => 'absent',
     path   => '/usr/local/bin/get_fact_value',
