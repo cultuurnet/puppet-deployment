0102bfca55b52b72625055e61de052649d418f12
Kristof Willaert <kristof.willaert@cultuurnet.be>
2020-02-06 16:09:20 +0100

    We don't need public/private keys for jwtprovider on auth0

--- manifests/udb3/jwtprovider.pp
+++ manifests/udb3/jwtprovider.pp
@@ -1,7 +1,5 @@
 class deployment::udb3::jwtprovider (
   $config_source,
-  $privkey_source,
-  $pubkey_source,
   $project_prefix = 'udb3',
   $noop_deploy = false,
   $puppetdb_url = undef
@@ -32,26 +30,6 @@ class deployment::udb3::jwtprovider (
     owner   => 'www-data',
     group   => 'www-data',
     recurse => true,
-    require => 'Package[udb3-jwt-provider]',
-    noop    => $noop_deploy
-  }
-
-  file { 'udb3-jwtprovider-privkey':
-    ensure  => 'file',
-    path    => "${basedir}/private.pem",
-    source  => $privkey_source,
-    owner   => 'www-data',
-    group   => 'www-data',
-    require => 'Package[udb3-jwt-provider]',
-    noop    => $noop_deploy
-  }
-
-  file { 'udb3-jwtprovider-pubkey':
-    ensure  => 'file',
-    path    => "${basedir}/public.pem",
-    source  => $pubkey_source,
-    owner   => 'www-data',
-    group   => 'www-data',
     require => 'Package[udb3-jwt-provider]',
     noop    => $noop_deploy
   }
