989477f16b44b5036dd13fb3bb1104a4a9fda4d9
Kristof Willaert <kristof.willaert@cultuurnet.be>
2020-02-06 15:34:03 +0100

    Provide 2 JWT classes (jwt for uitidv1, jwtprovider for auth0)

--- manifests/udb3/jwt.pp
+++ manifests/udb3/jwt.pp
@@ -1,4 +1,4 @@
-class deployment::udb3::jwtprovider (
+class deployment::udb3::jwt (
   $config_source,
   $privkey_source,
   $pubkey_source,
@@ -7,60 +7,60 @@ class deployment::udb3::jwtprovider (
   $puppetdb_url = undef
 ) {
 
-  $basedir = '/var/www/udb-jwt-provider'
+  $basedir = '/var/www/udb-jwt'
 
-  package { 'udb3-jwt-provider':
+  package { 'udb3-jwt':
     ensure => 'latest',
     notify => 'Class[Apache::Service]',
     noop   => $noop_deploy
   }
 
-  file { 'udb3-jwtprovider-config':
+  file { 'udb3-jwt-config':
     ensure  => 'file',
     path    => "${basedir}/config.yml",
     source  => $config_source,
     owner   => 'www-data',
     group   => 'www-data',
-    require => 'Package[udb3-jwt-provider]',
+    require => 'Package[udb3-jwt]',
     notify  => 'Class[Apache::Service]',
     noop    => $noop_deploy
   }
 
-  file { 'udb3-jwtprovider-log':
+  file { 'udb3-jwt-log':
     ensure  => 'directory',
     path    => "${basedir}/log",
     owner   => 'www-data',
     group   => 'www-data',
     recurse => true,
-    require => 'Package[udb3-jwt-provider]',
+    require => 'Package[udb3-jwt]',
     noop    => $noop_deploy
   }
 
-  file { 'udb3-jwtprovider-privkey':
+  file { 'udb3-jwt-privkey':
     ensure  => 'file',
     path    => "${basedir}/private.pem",
     source  => $privkey_source,
     owner   => 'www-data',
     group   => 'www-data',
-    require => 'Package[udb3-jwt-provider]',
+    require => 'Package[udb3-jwt]',
     noop    => $noop_deploy
   }
 
-  file { 'udb3-jwtprovider-pubkey':
+  file { 'udb3-jwt-pubkey':
     ensure  => 'file',
     path    => "${basedir}/public.pem",
     source  => $pubkey_source,
     owner   => 'www-data',
     group   => 'www-data',
-    require => 'Package[udb3-jwt-provider]',
+    require => 'Package[udb3-jwt]',
     noop    => $noop_deploy
   }
 
   profiles::deployment::versions { $title:
     project      => $project_prefix,
-    packages     => 'udb3-jwt-provider',
+    packages     => 'udb3-jwt',
     puppetdb_url => $puppetdb_url
   }
 
-  Class['php'] -> Class['deployment::udb3::jwtprovider']
+  Class['php'] -> Class['deployment::udb3::jwt']
 }
