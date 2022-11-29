b8cd2372ff25014bd13475484750e276a3509382
Kristof Willaert <kristof@publiq.be>
2022-11-29 10:51:55 +0100

    Remove deployment of search_test_consumer

--- manifests/udb3/search_test_consumer.pp
+++
@@ -1,9 +0,0 @@
-class deployment::udb3::search_test_consumer {
-
-  package { 'sapi3-test-consumer':
-    ensure => 'latest',
-    notify => Class['Apache::Service']
-  }
-
-  Class['php'] -> Class['deployment::udb3::search_test_consumer']
-}
