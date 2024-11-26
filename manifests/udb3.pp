c0d77eb0cd8794abbe19e8e84b1eacd0c48ae394
Kristof Willaert <kristof@publiq.be>
2024-11-26 17:29:12 +0100

    Remove unused UiTdatabank frontend deployment classes

--- manifests/udb3.pp
+++ manifests/udb3.pp
@@ -1,19 +1,11 @@
 class deployment::udb3 (
   $with_entry_api         = true,
-  $with_angular           = true,
-  $with_frontend          = true,
   $with_jwtprovider       = true
 ){
 
   unless $facts['noop_deploy'] == 'true' {
     if $with_entry_api {
       contain deployment::udb3::entry_api
-    }
-    if $with_angular {
-      contain deployment::udb3::angular
-    }
-    if $with_frontend {
-      contain deployment::udb3::frontend
     }
     if $with_jwtprovider {
       contain deployment::udb3::jwtprovider
